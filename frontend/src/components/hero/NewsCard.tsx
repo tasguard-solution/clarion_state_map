44af4b02d87c808c9c84f2973e6066ca30c45105
Anointing Tamunowunari-Tasker <anointingtasker2002@gmail.com>
2026-04-12 01:02:16 +0100

    feat: implement Footer component with links and social media icons

---
+++ frontend/src/components/hero/NewsCard.tsx
@@ -0,0 +1,3 @@
+function NewsCard() {}
+
+export default NewsCard;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import "./Home.css";
+import Footer from "../components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Footer />
     </>
   );
 }
